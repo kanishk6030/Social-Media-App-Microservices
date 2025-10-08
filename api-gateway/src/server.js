c28af8c01ea3591a1f98d07d0bdb9f83bc0e08f4
Kanishka Vats <kanishkvats2020@gmail.com>
2025-10-08 23:02:12 +0530

    added the proxy

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -7,6 +7,8 @@ const {rateLimit} = require("express-rate-limit");
 const {RedisStore} = require("rate-limit-redis");
 const logger = require("./utils/logger");
 const proxy = require("express-http-proxy");
+const errorHandler = require("./middleware/errorHandler")
+
 
 const app = express();
 
@@ -19,7 +21,7 @@ app.use(helmet());
 app.use(express.json());
 
 //rate limitng 
-const ratelimit = rateLimit({
+const ratelimitOptions = rateLimit({
     windowMs: 15 * 60 * 1000,
     max:50,
     standardHeaders:true,
@@ -36,10 +38,44 @@ const ratelimit = rateLimit({
     })
 })
 
-app.use(rateLimit);
+app.use(ratelimitOptions);
 
 app.use((req,res,next)=>{
     logger.info(`Recieved ${req.method} request to ${req.url}`);
     logger.info(`Request body,${req.body}`);
     next();
+})
+
+const proxyOptions = {
+    proxyReqPathResolver : (req) =>{
+        return req.originalUrl.replace(/^\/v1/,"/api")
+    },
+    proxyErrorHandler: (err,res,next) =>{
+        logger.warn(`Proxy Error : ${err.message}`);
+        res.status(400).json({
+            success:false,
+            message:"Internal Server Error",
+            error:err.message,
+        })
+    }
+}
+// Setting up the proxy for the Identity Service
+app.use("/v1/auth",proxy(process.env.IDENTITY_SERVICE_URL,{
+    ...proxyOptions,
+    proxyReqOptDecorator:(proxyReqOpts, srcReq) =>{
+         proxyReqOpts.headers['Content-Type'] = 'application/json';
+         return proxyReqOpts;
+    },
+    userResDecorator:(proxyRes, proxyResData, userReq, userRes)=>{
+        logger.info(`Response recieved from Identity Service ${proxyRes.statusCode}`);
+        return proxyResData;
+    }
+}));
+
+app.use(errorHandler);
+
+app.listen(PORT,()=>{
+    logger.info(`API gateway is running on the PORT ${PORT}`);
+    logger.info(`Identity Service is Running on the PORT ${process.env.IDENTITY_SERVICE_URL}`);
+    logger.info(`Redis URL ${process.env.REDIS_URL}`)
 })
